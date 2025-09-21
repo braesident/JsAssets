93b4f725e94e7e07350749e987bd67c2b0afd525
braesident <braesie@hotmail.de>
2025-09-21 16:25:06 +0200

    initial commit

---
+++ dev.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use exunova\JsAsset\JsAsset;
+
+// require_once __DIR__.'/src/JsAsset.php';
+require_once __DIR__.'/vendor/autoload.php';
+// var_dump(JsAsset::getAssets());
+// var_dump(JsAsset::getScriptTagArray('/asset', 'test', 'dropdown'));
+echo JsAsset::getScriptTags('/asset', 'test', 'dropdown');
+$asset = JsAsset::getAsset('/asset/dropdown.js', true);
+echo "\n{$asset}";
---
+++ src/JsAsset.php
@@ -0,0 +1,157 @@
+<?php
+
+declare(strict_types=1);
+
+namespace exunova\JsAsset;
+
+use DirectoryIterator;
+use MatthiasMullie\Minify\JS as MinifyJS;
+
+final class JsAsset
+{
+  private const IGNORE_REPOS = ['php'];
+
+  private const RAW_SCRIPT_TAG = '<script type="text/javascript" src="%1$s"></script>';
+
+  /**
+   * Return js asset files.
+   */
+  public static function getAssets(): array
+  {
+    $assets = [];
+
+    foreach (new DirectoryIterator(\dirname(__DIR__, 1).'/assets/') as $fileinfo) {
+      if ('js' !== $fileinfo->getExtension()) {
+        continue;
+      }
+
+      $fileKey = $fileinfo->getBasename('.js');
+
+      $assets[] = $fileKey;
+    }
+
+    $jcomposer = file_get_contents(\dirname(__DIR__, 1).'/composer.json');
+    $composer  = json_decode($jcomposer);
+
+    foreach ($composer->require as $repo => $_version) {
+      if (\in_array($repo, self::IGNORE_REPOS, true)) {
+        continue;
+      }
+      $repoSet = explode('/', $repo);
+      if (\count($repoSet) < 2) {
+        continue;
+      }
+
+      $repoPath = \dirname(__DIR__, 1)."/vendor/{$repoSet[0]}/{$repoSet[1]}/";
+
+      foreach (new DirectoryIterator($repoPath) as $fileinfo) {
+        if ('js' !== $fileinfo->getExtension()) {
+          continue;
+        }
+
+        $fileKey = $fileinfo->getBasename('.js');
+        if (\in_array($fileKey, $assets, true)) {
+          $fileKey = "{$repoSet[0]}.{$repoSet[1]}.{$fileKey}";
+        }
+
+        $assets[] = $fileKey;
+      }
+    }
+
+    return $assets;
+  }
+
+  public static function getScriptTagArray(string $path = '', string ...$matches): array
+  {
+    $assets = self::getAssets();
+    $tags   = [];
+    $path   = rtrim($path, '/');
+
+    foreach ($assets as $asset) {
+      if ($matches) {
+        if (\in_array($asset, $matches, true)) {
+          $tags[] = \sprintf(self::RAW_SCRIPT_TAG, $path.'/'.$asset.'.js');
+        }
+      } else {
+        $tags[] = \sprintf(self::RAW_SCRIPT_TAG, $path.'/'.$asset.'.js');
+      }
+    }
+
+    return $tags;
+  }
+
+  public static function getScriptTags(string $path = '', string ...$matches): string
+  {
+    $tags = self::getScriptTagArray($path, ...$matches);
+
+    return implode("\n", $tags);
+  }
+
+  public static function getAsset(string $path, bool $minify = false): string
+  {
+    $assets = [];
+    $path   = preg_replace('/\.js$/', '', $path);
+    $pathes = explode('/', $path);
+
+    $path = array_pop($pathes);
+
+    foreach (new DirectoryIterator(\dirname(__DIR__, 1).'/assets/') as $fileinfo) {
+      if ('js' !== $fileinfo->getExtension()) {
+        continue;
+      }
+
+      $fileKey = $fileinfo->getBasename('.js');
+
+      if ($path === $fileKey) {
+        return self::loadContent($fileinfo->getRealPath(), $minify);
+      }
+
+      $assets[] = $fileKey;
+    }
+
+    $jcomposer = file_get_contents(\dirname(__DIR__, 1).'/composer.json');
+    $composer  = json_decode($jcomposer);
+
+    foreach ($composer->require as $repo => $_version) {
+      if (\in_array($repo, self::IGNORE_REPOS, true)) {
+        continue;
+      }
+      $repoSet = explode('/', $repo);
+      if (\count($repoSet) < 2) {
+        continue;
+      }
+
+      $repoPath = \dirname(__DIR__, 1)."/vendor/{$repoSet[0]}/{$repoSet[1]}/";
+
+      foreach (new DirectoryIterator($repoPath) as $fileinfo) {
+        if ('js' !== $fileinfo->getExtension()) {
+          continue;
+        }
+
+        $fileKey = $fileinfo->getBasename('.js');
+        if (\in_array($fileKey, $assets, true)) {
+          $fileKey = "{$repoSet[0]}.{$repoSet[1]}.{$fileKey}";
+        }
+
+        if ($path === $fileKey) {
+          return self::loadContent($fileinfo->getRealPath(), $minify);
+        }
+
+        $assets[] = $fileKey;
+      }
+    }
+
+    return '';
+  }
+
+  private static function loadContent(string $path, bool $minify): string
+  {
+    if ($minify) {
+      $minifier = new MinifyJS($path);
+
+      return $minifier->minify();
+    }
+
+    return file_get_contents($path);
+  }
+}
